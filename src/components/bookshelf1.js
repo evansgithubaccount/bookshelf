60f5a5f8be11ce5b387eed02887e215d093397a2
Evan Moore <evanmoore234@gmail.com>
2019-04-19 15:42:38 -0400

    3rd commit

---
+++ src/components/bookshelf1.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import '../css/bookshelf1.css'
+
+class Bookshelf extends Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+       return ( <div>
+            <div className="shelf">
+                <div className="row">
+                    
+                </div>
+            </div>
+        </div>
+       )
+    }
+}
+
+export default Bookshelf;
