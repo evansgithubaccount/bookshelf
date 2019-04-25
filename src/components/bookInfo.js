fd2cf4434be9b3a669a63b4174f57e3815313a18
Evan Moore <evanmoore234@gmail.com>
2019-04-25 09:25:29 -0400

    passed props

--- src/components/bookInfo.js
+++ src/components/bookInfo.js
@@ -4,7 +4,7 @@ import '../css/bookInfo.css';
 const BookInformation = (props) => {
     const title = props.location.state.title;
     const author = props.location.state.author;
-    const summary = props.location.state.summary
+    const summary = props.location.state.summary;
     return (
     <div>
         <h1>{title} by {author}</h1>
